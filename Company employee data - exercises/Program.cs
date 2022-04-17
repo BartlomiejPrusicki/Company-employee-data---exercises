087ca05b1b5d6c9bac110a092cec908d86da2190
BartlomiejPrusicki <91379817+BartlomiejPrusicki@users.noreply.github.com>
2022-04-17 18:17:16 +0200

    Add project files.

---
+++ Company employee data - exercises/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Company employee data");
+Console.WriteLine("");
+string name, lastname, sex;
+byte age;
+ulong pesel;
+uint numberEmployee;
+
+name = "Boguś";
+lastname = "von Sosonowiec";
+age = 30;
+sex = "M";
+pesel = 9223330576;
+numberEmployee = 2509324094;
+
+Console.WriteLine($"Name: {name}\r\nLastname: {lastname}\r\nAge: {age}\r\nSex: {sex}\r\nPesel: {pesel}\r\nNumber employee: {numberEmployee}");
+
